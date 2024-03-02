787be58ca7e516a942aff89b8ec75e9b8b724c06
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-02 15:30:13 -0600

    refactor database

--- src/api/v1/services/database.ts
+++ src/api/v1/services/database.ts
@@ -1,22 +1,14 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import mongoose from 'mongoose';
 import "dotenv/config";
 
 const mongoDBUrl = process.env.MONGODB_URL;
 if (!mongoDBUrl) { throw new Error("MONGODB_URL environment variable is not set.")};
 
-const client = new MongoClient(mongoDBUrl, {
-  serverApi: ServerApiVersion.v1,
-});
-
 export const connectDatabase = async () => {
-  try {
-    await client.connect();
+  try{
+    await mongoose.connect(mongoDBUrl)
     console.log('Successfully connected to MongoDB.');
-  } catch (err) {
-    // log error & throw error
+  } catch (err){
     console.error('Error connecting to MongoDB:', err);
-    throw new Error('Failed to connect to MongoDB');
   }
-  // always return mongoclient
-  return client;
-};
+}
