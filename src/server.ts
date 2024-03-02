a87c77d38666416a2819557738f2738f4515ee5a
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-01 19:06:19 -0600

    update error handler

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import express, { Express} from 'express';
+import express, { Express, Request, Response } from 'express';
 import { connectDatabase } from './api/v1/services/database';
 import urlRouter from './api/v1/routes/urlRouter';
-import { globalErrorHandler, notFoundErrorHandler } from './api/v1/middleware/errorHandlers';
+import { globalErrorHandler } from './api/v1/middleware/globalErrorHandler';
 
 const app: Express = express();
 
@@ -14,8 +14,10 @@ connectDatabase();
 // route users to urls router
 app.use("/api/v1/url", urlRouter );
 
-// route users to catch 404 error handler
-app.use(notFoundErrorHandler);
+// catch 404 error handler
+app.use((req : Request, res : Response) => {
+  res.status(404).json({ error: 'Not Found' });
+});
 
 // global error handler
 app.use(globalErrorHandler);
