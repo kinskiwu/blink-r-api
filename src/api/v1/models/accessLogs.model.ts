9e3544b531fcacffc7b4f2d8b97e31bb011e1821
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-02 20:23:54 -0600

    update acccesslogs model to time series

--- src/api/v1/models/accessLogs.model.ts
+++ src/api/v1/models/accessLogs.model.ts
@@ -1,9 +1,17 @@
 import mongoose, { Schema } from 'mongoose';
 import { AccessLog } from '../types/DbModelTypes';
 
-const AccessLogSchema = new Schema({
-  accessTime: { type: Date, required: true, default: Date.now },
-  shortUrlId: { type: String, required: true }
-});
+const AccessLogSchema = new Schema(
+  {
+    accessTime: { type: Date, required: true, default: Date.now },
+    shortUrlId: { type: String, required: true },
+  },
+  {
+    timeseries: {
+      timeField: 'accessTime',
+      metaField: 'shortUrlId'
+    },
+  }
+);
 
 export const AccessLogModel = mongoose.model<AccessLog>('AccessLog', AccessLogSchema);
