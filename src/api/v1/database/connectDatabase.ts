cf0e7d2f0e07e2b6d4a7ba4abe9056a7fd07cc68
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-02 22:23:19 -0600

    move connectDatabase to database file

--- src/api/v1/database/connectDatabase.ts
+++ src/api/v1/database/connectDatabase.ts
@@ -2,10 +2,12 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 
 const mongoDBUrl = process.env.MONGODB_URL;
+// throw error if env variable for mongodb url not set
 if (!mongoDBUrl) {
   throw new Error('MONGODB_URL environment variable is not set.');
 }
 
+// function to connect to mongodb
 export const connectDatabase = async () => {
   try {
     await mongoose.connect(mongoDBUrl);
