a17ddb910948e616ed525c6f5a9e4520f3cc9fe2
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-04 01:28:19 -0600

    add comment to find or create shortrul service

--- src/api/v1/services/urlServices.ts
+++ src/api/v1/services/urlServices.ts
@@ -5,14 +5,19 @@ import { UrlModel } from '../models/urls.model';
 /**
  * Generates a short URL identifier.
  *
- * @param {string} uniqueId - An optional parameter to provide a custom unique identifier.
- *                            If not provided, a new uuid will be generated.
+ * @param {string} uniqueId - An optional parameter to provide a custom unique identifier. If not provided, a new uuid will be generated.
  * @returns {string} - The base62 encoded string of the uniqueId, acting as a short URL identifier.
  */
 export const generateShortUrl = (uniqueId: string = uuid()): string => {
   return encodeToBase62(uniqueId);
 };
 
+/**
+ * Retrieves an existing short URL or creates a new one for a given long URL.
+ *
+ * @param longUrl The original long URL to shorten.
+ * @returns A promise that resolves to the full short URL path.
+ */
 export const findOrCreateShortUrl = async (longUrl: string) => {
   let urlDocument = await UrlModel.findOne({ longUrl });
   let shortUrlId;
