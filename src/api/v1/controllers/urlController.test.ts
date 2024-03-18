9149dacd5a6114acbc9f9afefc31892ac3f6b429
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-17 22:52:45 -0500

    refactor url controller tests

--- src/api/v1/controllers/urlController.test.ts
+++ src/api/v1/controllers/urlController.test.ts
@@ -1,235 +1,103 @@
 import { Request, Response, NextFunction } from 'express';
-import { findOrCreateShortUrl } from '../services/urlServices';
+import {
+  findOrCreateShortUrl,
+  getAccessCountForShortUrl,
+} from '../services/urlServices';
 import {
   createShortUrl,
   generateAnalytics,
   redirectToLongUrl,
 } from './urlController';
-import { UrlModel } from '../models/urls.model';
-import { AccessLogModel } from '../models/accessLogs.model';
+import { RedisClientType } from 'redis';
 
-jest.mock('../services/urlServices', () => ({
-  findOrCreateShortUrl: jest.fn(),
-}));
-jest.mock('../models/urls.model', () => ({
-  UrlModel: {
-    findOne: jest.fn(),
-  },
-}));
-jest.mock('../models/accessLogs.model', () => ({
-  AccessLogModel: {
-    aggregate: jest.fn(),
-  },
-}));
-jest.mock('../../utils/helpers', () => ({
-  calculateStartDate: jest.fn(),
+// Mock external modules
+jest.mock('../services/urlServices');
+jest.mock('../models/accessLogs.model');
+jest.mock('redis', () => ({
+  createClient: jest.fn().mockReturnThis(),
+  get: jest.fn(),
+  set: jest.fn(),
 }));
 
 describe('URL Controller Tests', () => {
+  let mockReq: any;
+  let mockRes: Partial<Response>;
+  let nextFunction: NextFunction = jest.fn();
+  const mockRedisClient: Partial<RedisClientType> = {
+    get: jest.fn(),
+    set: jest.fn(),
+  };
+
   beforeEach(() => {
     jest.clearAllMocks();
-  });
-
-  afterEach(() => {
-    jest.resetAllMocks();
-  });
-
-  afterAll(() => {
-    jest.restoreAllMocks();
+    mockReq = { app: { locals: { redisClient: mockRedisClient } } };
+    mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockReturnThis(),
+      redirect: jest.fn().mockReturnThis(),
+    };
+    nextFunction = jest.fn();
   });
 
   describe('createShortUrl Controller', () => {
-    let req: Partial<Request>;
-    let res: Partial<Response>;
-    let next: jest.Mock<NextFunction>;
-
-    beforeEach(() => {
-      req = { body: {} };
-      res = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn().mockReturnThis(),
-        redirect: jest.fn().mockReturnThis(),
-      };
-      next = jest.fn();
-    });
-
-    it('should successfully create a short URL for a valid long URL', async () => {
+    it('should successfully create and return a short URL', async () => {
       const mockLongUrl = 'http://cloudflare.com';
       const mockShortUrlId = 'cloudflare';
       (findOrCreateShortUrl as jest.Mock).mockResolvedValue(mockShortUrlId);
-      req.body.longUrl = mockLongUrl;
+      mockReq.body = { longUrl: mockLongUrl };
 
-      await createShortUrl(req as Request, res as Response, next);
+      await createShortUrl(
+        mockReq as Request,
+        mockRes as Response,
+        nextFunction
+      );
 
       expect(findOrCreateShortUrl).toHaveBeenCalledWith(mockLongUrl);
-      expect(res.status).toHaveBeenCalledWith(201);
-      expect(res.json).toHaveBeenCalledWith({
+      expect(mockRes.status).toHaveBeenCalledWith(201);
+      expect(mockRes.json).toHaveBeenCalledWith({
         shortUrl: `www.shorturl.com/${mockShortUrlId}`,
       });
-    });
-
-    it('should handle server error when URL creation fails', async () => {
-      const mockError = new Error('Server error');
-      (findOrCreateShortUrl as jest.Mock).mockRejectedValue(mockError);
-      req.body.longUrl = 'http://cloudflare.com';
-
-      await createShortUrl(req as Request, res as Response, next);
-
-      expect(findOrCreateShortUrl).toHaveBeenCalledWith(
-        'http://cloudflare.com'
-      );
-      expect(next).toHaveBeenCalledWith({
-        status: 500,
-        message: 'Server error encountered while creating short URL.',
-        err: mockError,
-      });
-    });
-
-    it('should call next with an error if findOrCreateShortUrl does not return a URL', async () => {
-      const mockLongUrl = 'http://cloudflare.com';
-      (findOrCreateShortUrl as jest.Mock).mockResolvedValue(null);
-      req.body.longUrl = mockLongUrl;
-
-      await createShortUrl(req as Request, res as Response, next);
-
-      expect(findOrCreateShortUrl).toHaveBeenCalledWith(mockLongUrl);
-      expect(next).toHaveBeenCalledWith(expect.anything());
     });
   });
 
   describe('redirectToLongUrl Controller', () => {
-    let req: Partial<Request>;
-    let res: Partial<Response>;
-    let next: jest.Mock<NextFunction>;
+    it('should redirect to long URL when short URL ID is valid', async () => {
+      const mockShortUrlId = 'cloudflare';
+      const mockLongUrl = 'http://cloudflare.com';
+      mockReq.params = { shortUrlId: mockShortUrlId };
+      (mockRedisClient.get as jest.Mock).mockResolvedValue(mockLongUrl);
 
-    beforeEach(() => {
-      jest.clearAllMocks();
-      req = { params: { shortUrlId: '' } };
-      res = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn().mockReturnThis(),
-        redirect: jest.fn().mockReturnThis(),
-      };
-      next = jest.fn();
-    });
-    // todo: debug line 118
-    it('redirects to the corresponding long URL for a valid shortUrlId', async () => {
-      const mockShortUrlId = 'valid';
-      const mockLongUrl = 'https://cloudflare.com';
-      req.params!.shortUrlId = mockShortUrlId;
-
-      (UrlModel.findOne as jest.Mock).mockResolvedValue({
-        longUrl: mockLongUrl,
-        shortUrls: [{ shortUrlId: mockShortUrlId }],
-      });
-
-      await redirectToLongUrl(req as Request, res as Response, next);
-
-      expect(UrlModel.findOne).toHaveBeenCalledWith({
-        'shortUrls.shortUrlId': { $eq: mockShortUrlId },
-      });
-
-      // expect(res.redirect).toHaveBeenCalledWith(301, mockLongUrl);
-    });
-
-    it('returns a 400 error if the short URL is not found', async () => {
-      req.params!.shortUrlId = 'invalidShortId';
-      (UrlModel.findOne as jest.Mock).mockResolvedValue(null);
-
-      await redirectToLongUrl(req as Request, res as Response, next);
-
-      expect(UrlModel.findOne).toHaveBeenCalled();
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.json).toHaveBeenCalledWith({ error: 'Short URL not found' });
-    });
-
-    it('handles server errors gracefully', async () => {
-      const mockError = new Error('Test Error');
-      req.params!.shortUrlId = 'shortIdWithError';
-      (UrlModel.findOne as jest.Mock).mockRejectedValue(mockError);
-
-      await redirectToLongUrl(req as Request, res as Response, next);
-
-      expect(next).toHaveBeenCalledWith(
-        expect.objectContaining({
-          status: 500,
-          message: 'Server error encountered while redirecting short URL.',
-        })
+      await redirectToLongUrl(
+        mockReq as Request,
+        mockRes as Response,
+        nextFunction
       );
+
+      expect(mockRes.redirect).toHaveBeenCalledWith(301, mockLongUrl);
     });
   });
 
   describe('generateAnalytics Controller', () => {
-    let req: Partial<Request>;
-    let res: Partial<Response>;
-    let next: jest.Mock<NextFunction>;
-
-    beforeEach(() => {
-      req = { query: {} };
-      res = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn().mockReturnThis(),
-      };
-      next = jest.fn();
-    });
-
-    it('successfully returns analytics data for a valid short URL and time frame', async () => {
-      req.query = { shortUrlId: 'validShortId', timeFrame: '24h' };
-      const mockAccessData = [{ accessCount: 5 }];
-      (UrlModel.findOne as jest.Mock).mockResolvedValue(true);
-      (AccessLogModel.aggregate as jest.Mock).mockResolvedValue(mockAccessData);
-
-      await generateAnalytics(req as Request, res as Response, next);
-
-      expect(UrlModel.findOne).toHaveBeenCalled();
-      expect(AccessLogModel.aggregate).toHaveBeenCalled();
-      expect(res.status).toHaveBeenCalledWith(200);
-      expect(res.json).toHaveBeenCalledWith({
-        timeFrame: '24h',
-        accessCount: 5,
-      });
-    });
-
-    it('returns a 400 error if the short URL is not found', async () => {
-      req.query = { shortUrlId: 'nonexistentId', timeFrame: '7d' };
-      (UrlModel.findOne as jest.Mock).mockResolvedValue(null);
-
-      await generateAnalytics(req as Request, res as Response, next);
-
-      expect(UrlModel.findOne).toHaveBeenCalled();
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.json).toHaveBeenCalledWith({
-        message: 'Short URL not found.',
-      });
-    });
-
-    it('handles server errors gracefully', async () => {
-      req.query = { shortUrlId: 'errorId', timeFrame: '24h' };
-      const error = new Error('Test error');
-      (UrlModel.findOne as jest.Mock).mockRejectedValue(error);
-
-      await generateAnalytics(req as Request, res as Response, next);
-
-      expect(next).toHaveBeenCalledWith(
-        expect.objectContaining({
-          status: 500,
-          message: 'Server error encountered while generating analytics.',
-          err: error,
-        })
+    it('should return analytics data for a given short URL', async () => {
+      const mockShortUrlId = 'cloudflare';
+      const mockTimeFrame = '24h';
+      const mockAccessCount = 5;
+      mockReq.query = { shortUrlId: mockShortUrlId, timeFrame: mockTimeFrame };
+      (mockRedisClient.get as jest.Mock).mockResolvedValue(null);
+      (getAccessCountForShortUrl as jest.Mock).mockResolvedValue(
+        mockAccessCount
       );
-    });
 
-    it('returns analytics with default time frame when an unsupported time frame is provided', async () => {
-      req.query = { shortUrlId: 'validShortId', timeFrame: 'unsupported' };
-      const mockAccessData = [{ accessCount: 10 }];
-      (UrlModel.findOne as jest.Mock).mockResolvedValue(true);
-      (AccessLogModel.aggregate as jest.Mock).mockResolvedValue(mockAccessData);
+      await generateAnalytics(
+        mockReq as Request,
+        mockRes as Response,
+        nextFunction
+      );
 
-      await generateAnalytics(req as Request, res as Response, next);
-
-      expect(res.json).toHaveBeenCalledWith({
-        timeFrame: 'unsupported',
-        accessCount: 10,
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.json).toHaveBeenCalledWith({
+        timeFrame: mockTimeFrame,
+        accessCount: mockAccessCount,
       });
     });
   });
