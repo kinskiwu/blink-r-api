25769e40eb14df3edd54515efe7d302c9ddfc663
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-18 00:43:19 -0500

    add winston

--- src/startServer.ts
+++ src/startServer.ts
@@ -3,6 +3,17 @@ import 'dotenv/config';
 import { connectDatabase } from './api/v1/database/connectDatabase';
 import mongoose from 'mongoose';
 import { connectRedis } from './api/v1/database/connectRedis';
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
 
 const PORT = process.env.PORT || 4000;
 /**
@@ -16,34 +27,34 @@ const startServer = async () => {
     app.locals.redisClient = redisClient;
 
     const server = app.listen(PORT, () =>
-      console.log(`Listening on port ${PORT}`)
+      logger.info(`Listening on port ${PORT}`)
     );
 
     // handle server shutdown with SIGINT signal (e.g Ctrl+C)
     process.on('SIGINT', async () => {
-      console.log('SIGINT signal received: closing MongoDB connection');
+      logger.info('SIGINT signal received: closing MongoDB connection');
 
       try {
         await mongoose.disconnect();
-        console.log('MongoDB connection closed.');
+        logger.info('MongoDB connection closed.');
       } catch (err) {
-        console.error('Error closing MongoDB connection:', err);
+        logger.error('Error closing MongoDB connection:', err);
       }
 
       try {
         await redisClient.quit();
-        console.log('Redis connection closed.');
+        logger.info('Redis connection closed.');
       } catch (err) {
-        console.error('Error closing Redis connection:', err);
+        logger.error('Error closing Redis connection:', err);
       }
 
       server.close(() => {
-        console.log('Server shut down successfully.');
+        logger.info('Server shut down successfully.');
         process.exit(0);
       });
     });
   } catch (err) {
-    console.error('Error starting server:', err);
+    logger.error('Error starting server:', err);
     process.exit(1);
   }
 };
@@ -51,7 +62,7 @@ const startServer = async () => {
 // prevent server initialization during test runs to avoid jest conflicts
 if (process.env.NODE_ENV !== 'test') {
   startServer().catch((err) => {
-    console.error('Error starting server:', err);
+    logger.error('Error starting server:', err);
     process.exit(1);
   });
 }
