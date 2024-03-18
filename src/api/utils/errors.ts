362abf8fe882fa1166fc9134cc469c682a71a1f0
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-17 21:07:44 -0500

    add database & cache error classes

--- src/api/utils/errors.ts
+++ src/api/utils/errors.ts
@@ -4,3 +4,17 @@ export class NotFoundError extends Error {
     this.name = 'NotFoundError';
   }
 }
+
+export class DatabaseError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'DatabaseError';
+  }
+}
+
+export class CacheError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'CacheError';
+  }
+}
