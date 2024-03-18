cc7523dc6f77a019bb1cb2d3d96efef5db2e660a
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-17 21:44:30 -0500

    refactor global error hanlder

--- src/api/v1/middleware/globalErrorHandler.ts
+++ src/api/v1/middleware/globalErrorHandler.ts
@@ -1,10 +1,11 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../../utils/errors';
 
-interface CustomError {
-  log: string;
-  status: number;
-  message: string;
-}
+// interface CustomError {
+//   log: string;
+//   status: number;
+//   message: string;
+// }
 
 /**
  * Global error handler for Express that intercepts and standardizes error responses.
@@ -15,20 +16,35 @@ interface CustomError {
  * @param {Request} req - Express Request object, not directly used here.
  * @param {Response} res - Express Response object for sending the error response.
  */
+// export const globalErrorHandler = (
+//   err: CustomError,
+//   req: Request,
+//   res: Response
+// ) => {
+//   const defaultErr: CustomError = {
+//     log: 'Express error handler caught unknown middleware error',
+//     status: 500,
+//     message: 'An error occurred',
+//   };
+
+//   // combine default error with custom props
+//   const errorObj = { ...defaultErr, ...err };
+//   console.log(errorObj.log);
+
+//   return res.status(errorObj.status).json(errorObj.message);
+// };
+
 export const globalErrorHandler = (
-  err: CustomError,
+  error: Error | CustomError,
   req: Request,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
-  const defaultErr: CustomError = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 500,
-    message: 'An error occurred',
-  };
-
-  // combine default error with custom props
-  const errorObj = { ...defaultErr, ...err };
-  console.log(errorObj.log);
-
-  return res.status(errorObj.status).json(errorObj.message);
+  if (error instanceof CustomError) {
+    console.error(`${error.constructor.name}: ${error.message}`);
+    return res.status(error.status).json({ error: error.message });
+  } else {
+    console.error(`Unexpected error: ${error.message}`);
+    return res.status(500).json({ error: 'An unexpected error occurred.' });
+  }
 };
