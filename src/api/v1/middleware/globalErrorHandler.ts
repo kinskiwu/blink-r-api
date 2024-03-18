591b39504d4a0d7d60239e21f67320c0927ff185
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-17 21:46:34 -0500

    remove commented out code

--- src/api/v1/middleware/globalErrorHandler.ts
+++ src/api/v1/middleware/globalErrorHandler.ts
@@ -1,12 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../../utils/errors';
 
-// interface CustomError {
-//   log: string;
-//   status: number;
-//   message: string;
-// }
-
 /**
  * Global error handler for Express that intercepts and standardizes error responses.
  * Logs error details for internal review and returns a structured error message to the client.
@@ -16,23 +10,6 @@ import { CustomError } from '../../utils/errors';
  * @param {Request} req - Express Request object, not directly used here.
  * @param {Response} res - Express Response object for sending the error response.
  */
-// export const globalErrorHandler = (
-//   err: CustomError,
-//   req: Request,
-//   res: Response
-// ) => {
-//   const defaultErr: CustomError = {
-//     log: 'Express error handler caught unknown middleware error',
-//     status: 500,
-//     message: 'An error occurred',
-//   };
-
-//   // combine default error with custom props
-//   const errorObj = { ...defaultErr, ...err };
-//   console.log(errorObj.log);
-
-//   return res.status(errorObj.status).json(errorObj.message);
-// };
 
 export const globalErrorHandler = (
   error: Error | CustomError,
