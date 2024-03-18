ea2323588ce60bacfd8e3003deda9245271e48c4
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-17 22:29:47 -0500

    minor typo

--- src/api/v1/middleware/globalErrorHandler.ts
+++ src/api/v1/middleware/globalErrorHandler.ts
@@ -1,27 +1,29 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { CustomError } from '../../utils/errors';
 
 /**
  * Global error handler for Express that intercepts and standardizes error responses.
  * Logs error details for internal review and returns a structured error message to the client.
- * Defaults are provided for missing error properties to ensure a consistent response format.
+ * This handler ensures a consistent response format for all errors.
  *
- * @param {CustomError} err - Error object potentially containing `log`, `status`, and `message`.
- * @param {Request} req - Express Request object, not directly used here.
- * @param {Response} res - Express Response object for sending the error response.
+ * @param err - Error object potentially containing `status` and `message`.
+ * @param req - Express Request object, not directly used here.
+ * @param res - Express Response object for sending the error response.
+ * @param next - Next function for passing execution to the next middleware (not used here but required for error handlers).
  */
 type ErrorHandlerError = Error | CustomError;
 
 export const globalErrorHandler = (
-  error: ErrorHandlerError,
+  err: ErrorHandlerError,
   req: Request,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
-  if (error instanceof CustomError) {
-    console.error(`${error.constructor.name}: ${error.message}`);
-    return res.status(error.status).json({ error: error.message });
+  if (err instanceof CustomError) {
+    console.error(`${err.constructor.name}: ${err.message}`);
+    return res.status(err.status).json({ err: err.message });
   } else {
-    console.error(`Unexpected error: ${error.message}`);
-    return res.status(500).json({ error: 'An unexpected error occurred.' });
+    console.error(`Unexpected error: ${err.message}`);
+    return res.status(500).json({ err: 'An unexpected error occurred.' });
   }
 };
