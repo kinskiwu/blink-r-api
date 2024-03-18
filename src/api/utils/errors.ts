4614f626a926a6ef1a0fddc5b498b9ce2dc4c549
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-17 21:44:16 -0500

    refactor error classes

--- src/api/utils/errors.ts
+++ src/api/utils/errors.ts
@@ -1,20 +1,32 @@
-export class NotFoundError extends Error {
-  constructor(message: string) {
+export class CustomError extends Error {
+  public status: number;
+  public message: string;
+
+  constructor(status: number, message: string) {
     super(message);
-    this.name = 'NotFoundError';
+    this.status = status;
+    this.message = message;
+    Object.setPrototypeOf(this, CustomError.prototype);
   }
 }
 
-export class DatabaseError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'DatabaseError';
+export class NotFoundError extends CustomError {
+  constructor(message: string = 'Not Found') {
+    super(404, message);
+    Object.setPrototypeOf(this, NotFoundError.prototype);
   }
 }
 
-export class CacheError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'CacheError';
+export class DatabaseError extends CustomError {
+  constructor(message: string = 'Database Error') {
+    super(500, message);
+    Object.setPrototypeOf(this, DatabaseError.prototype);
+  }
+}
+
+export class CacheError extends CustomError {
+  constructor(message: string = 'Cache Error') {
+    super(500, message);
+    Object.setPrototypeOf(this, CacheError.prototype);
   }
 }
