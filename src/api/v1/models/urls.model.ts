6eae83c6f63408fe022630f7abcf0c14c1ff0707
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-24 13:22:48 -0500

    add unique prop to shorturl id

--- src/api/v1/models/urls.model.ts
+++ src/api/v1/models/urls.model.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema } from 'mongoose';
 import { Url } from '../types/DbModelTypes';
 
 const ShortUrlSchema = new Schema({
-  _id: { type: String, required: true },
+  _id: { type: String, unique: true, required: true },
   createdAt: { type: Date, required: true, default: Date.now },
 });
 
