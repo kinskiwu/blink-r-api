6469fc5b7db64266a072e59fa2cd383859d71048
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-06-03 02:57:16 -0500

    fixed urls

--- src/tests/server.test.ts
+++ src/tests/server.test.ts
@@ -37,12 +37,12 @@ describe('GET / endpoint', () => {
   });
 });
 
-describe.skip('URL Shortening API', () => {
+describe('URL Shortening API', () => {
   describe('POST /api/v1/urls/shorten', () => {
     it('should create a short URL for a valid long URL', async () => {
       const longUrl = 'https://cloudflare.com/randomLongUrl';
       const response = await request(app)
-        .post('/api/v1/url/shorten')
+        .post('/api/v1/urls/shorten')
         .send({ longUrl });
 
       expect(response.status).toBe(201);
@@ -66,19 +66,19 @@ describe.skip('URL Shortening API', () => {
     });
   });
 
-  describe.skip('GET /api/v1/urls/:shortUrlId', () => {
+  describe('GET /api/v1/urls/:shortUrlId', () => {
     it('should return either 400 (or 404 if empty) when input an invalidShortUrlId', async () => {
       const invalidShortUrlId1 = 'thisIdDoesntExist';
       const invalidShortUrlId2 = '$123';
       const invalidShortUrlId3 = '';
       const response1 = await request(app).get(
-        `/api/v1/url/${invalidShortUrlId1}`
+        `/api/v1/urls/${invalidShortUrlId1}`
       );
       const response2 = await request(app).get(
-        `/api/v1/url/${invalidShortUrlId2}`
+        `/api/v1/urls/${invalidShortUrlId2}`
       );
       const response3 = await request(app).get(
-        `/api/v1/url/${invalidShortUrlId3}`
+        `/api/v1/urls/${invalidShortUrlId3}`
       );
 
       expect(response1.status).toBe(400);
@@ -87,12 +87,12 @@ describe.skip('URL Shortening API', () => {
     });
   });
 
-  describe.skip('GET /api/v1/urls/analytics', () => {
-    it('should return analytics data', async () => {
+  describe('GET /api/v1/urls/analytics', () => {
+    it.skip('should return analytics data', async () => {
       const shortUrlIdNotExist = 'Hv';
       const timeFrame = '24h';
       const response = await request(app)
-        .get('/api/v1/url/analytics')
+        .get('/api/v1/urls/analytics')
         .query({ shortUrlId: shortUrlIdNotExist, timeFrame });
       expect(response.status).toBe(400);
     });
