52f5d80ac12daaecd425c7a2e34805455f2adc08
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-06-02 21:04:47 -0500

    updated urlService

--- src/api/v1/services/urlService.ts
+++ src/api/v1/services/urlService.ts
@@ -8,103 +8,80 @@ import {
   DatabaseError,
   NotFoundError,
 } from '../../../config/errors';
+import { logger } from '../../../config/winston';
 
-class UrlService {
+export default class UrlService {
+  constructor(
+    private readonly urlModel = UrlModel,
+    private readonly accessLogModel = AccessLogModel,
+    private readonly logger = logger
+  ) {}
+
   /**
-   * Generates a short URL identifier.
-   *
-   * @param {string} uniqueId - An optional parameter to provide a custom unique identifier. If not provided, a new uuid will be generated.
-   * @returns {string} - The base62 encoded string of the uniqueId, acting as a short URL identifier.
+   * Generates a short URL identifier using a UUID and encodes it to Base62.
+   * @param uniqueId - An optional UUID. Defaults to a new UUID.
+   * @returns The Base62 encoded unique identifier.
    */
-  generateShortUrl(uniqueId: string = uuid()): string {
+  private encodeShortUrl(uniqueId: string = uuid()): string {
     return encodeToBase62(uniqueId);
   }
 
   /**
    * Retrieves an existing short URL or creates a new one for a given long URL.
-   *
-   * @param {string} longUrl The original long URL to shorten.
-   * @returns {Promise<string>} A promise that resolves to the full short URL path.
+   * @param originalLongUrl - The original long URL to shorten.
+   * @returns A promise that resolves to the short URL identifier.
    */
-  async findOrCreateShortUrl(longUrl: string): Promise<string> {
+  public async findOrCreateShortUrl(originalLongUrl: string): Promise<string> {
     try {
-      let urlDocument = await UrlModel.findOne({ longUrl: { $eq: longUrl } });
+      const existingUrlDocument = await this.findUrlByLongUrl(originalLongUrl);
 
-      let shortUrlId;
-
-      if (!urlDocument) {
-        const uniqueID = uuid();
-        shortUrlId = this.generateShortUrl(uniqueID);
-
-        urlDocument = new UrlModel({
-          longUrl,
-          shortUrls: [{ _id: shortUrlId }],
-        });
-
-        await urlDocument.save();
+      if (!existingUrlDocument) {
+        return this.createAndSaveNewUrl(originalLongUrl);
       } else {
-        shortUrlId = this.generateShortUrl();
-        urlDocument.shortUrls.push({ _id: shortUrlId });
-        await urlDocument.save();
+        const newShortUrlId = this.encodeShortUrl();
+        await this.addShortUrlToExistingUrl(existingUrlDocument, newShortUrlId);
+        return newShortUrlId;
       }
-
-      return shortUrlId;
-    } catch (err) {
-      if (err instanceof CustomError) {
-        throw err;
-      } else {
-        throw new DatabaseError(
-          'An error occurred while accessing the database'
-        );
-      }
+    } catch (error) {
+      this.handleServiceError(error as Error);
     }
   }
 
   /**
    * Searches for a URL document based on a provided short URL ID.
-   *
-   * @param {string} shortUrlId - The unique identifier for the short URL.
-   * @returns {Promise<Url>} A promise that resolves to the found URL document.
-   * @throws {NotFoundError} Throws this error if no document is found for the given short URL ID.
+   * @param shortUrlId - The unique identifier for the short URL.
+   * @returns A promise that resolves to the found URL document.
    */
-
-  async findShortUrl(shortUrlId): Promise<Url> {
+  public async findShortUrl(shortUrlId: string): Promise<Url> {
     try {
-      const urlDocument = await UrlModel.findOne({
+      const foundUrlDocument = await this.urlModel.findOne({
         'shortUrls._id': { $eq: shortUrlId },
       });
 
-      if (!urlDocument) {
+      if (!foundUrlDocument) {
         throw new NotFoundError('Short URL not found');
       }
 
-      return urlDocument;
-    } catch (err) {
-      if (err instanceof CustomError) {
-        throw err;
-      } else {
-        throw new DatabaseError(
-          'An error occurred while accessing the database'
-        );
-      }
+      return foundUrlDocument;
+    } catch (error) {
+      this.handleServiceError(error as Error);
     }
   }
 
   /**
    * Retrieves the access count for a given short URL within a specified timeframe.
-   *
-   * @param {string} shortUrlId The unique identifier of the short URL.
-   * @param {string}timeFrame The timeframe for which access count is being requested (e.g., "24h", "7d").
-   * @returns {Promise<number>}  A promise that resolves to the number of times the short URL was accessed within the specified timeframe.
+   * @param shortUrlId - The unique identifier of the short URL.
+   * @param timeFrame - The timeframe for which access count is being requested.
+   * @returns A promise that resolves to the number of times the short URL was accessed.
    */
-  async getAccessCountForShortUrl(
+  public async getAccessCountForShortUrl(
     shortUrlId: string,
     timeFrame: string
   ): Promise<number> {
     try {
       const startDate = calculateStartDate(timeFrame);
 
-      const accessCount = await AccessLogModel.aggregate([
+      const accessCountResult = await this.accessLogModel.aggregate([
         {
           $match: {
             shortUrlId,
@@ -116,11 +93,61 @@ class UrlService {
         },
       ]);
 
-      return accessCount.length > 0 ? accessCount[0].accessCount : 0;
+      return accessCountResult.length > 0 ? accessCountResult[0].accessCount : 0;
     } catch (error) {
+      this.handleServiceError(error as Error);
+    }
+  }
+
+  /**
+   * Finds a URL document by the long URL.
+   * @param longUrl - The long URL to search for.
+   * @returns A promise that resolves to the URL document, if found.
+   */
+  private async findUrlByLongUrl(longUrl: string): Promise<Url | null> {
+    return this.urlModel.findOne({ longUrl: { $eq: longUrl } });
+  }
+
+  /**
+   * Creates and saves a new URL document with a long URL and generated short URL.
+   * @param originalLongUrl - The original long URL to shorten.
+   * @returns A promise that resolves to the short URL identifier.
+   */
+  private async createAndSaveNewUrl(originalLongUrl: string): Promise<string> {
+    const newShortUrlId = this.encodeShortUrl();
+    const newUrlDocument = new this.urlModel({
+      longUrl: originalLongUrl,
+      shortUrls: [{ _id: newShortUrlId }],
+    });
+
+    await newUrlDocument.save();
+    return newShortUrlId;
+  }
+
+  /**
+   * Adds a new short URL to an existing URL document.
+   * @param existingUrlDocument - The existing URL document.
+   * @param newShortUrlId - The short URL identifier to add.
+   */
+  private async addShortUrlToExistingUrl(
+    existingUrlDocument: Url,
+    newShortUrlId: string
+  ): Promise<void> {
+    existingUrlDocument.shortUrls.push({ _id: newShortUrlId });
+    await existingUrlDocument.save();
+  }
+
+  /**
+   * Handles errors by throwing appropriate custom errors.
+   * @param serviceError - The error to handle.
+   * @throws CustomError - A custom error based on the provided error.
+   */
+  private handleServiceError(serviceError: Error): never {
+    this.logger.error(serviceError);
+    if (serviceError instanceof CustomError) {
+      throw serviceError;
+    } else {
       throw new DatabaseError('An error occurred while accessing the database');
     }
   }
 }
-
-export default new UrlService();
